6c2e7a2adc4d6a0fc129afd7b072c704e862ee0e
jayakumar-devops <ejayakumar09@gmail.com>
2025-08-21 20:54:39 +0530

    Updated_Lambda output

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda for GitHub Actions!')
+        'body': json.dumps('Hello from Lambda for GitHub Actions_Updated!')
     }
